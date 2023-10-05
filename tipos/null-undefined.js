a311ba51103e13890c81e73c382523ffa7f23300
Christian Araneda <132547458+ChristianAraneda@users.noreply.github.com>
2023-10-05 15:23:39 -0300

    null undefine

---
+++ tipos/null-undefined.js
@@ -0,0 +1,7 @@
+"use strict";
+(() => {
+    let isActive = null;
+    //   true, false, undefined, null
+    //   null !== undefined
+    console.log(isActive);
+})();
---
+++ tipos/null-undefined.ts
@@ -0,0 +1,8 @@
+(() => {
+  let isActive: boolean = null;
+
+  //   true, false, undefined, null
+  //   null !== undefined
+
+  console.log(isActive);
+})();
